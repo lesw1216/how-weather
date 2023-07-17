0fade812b966052fa8b3c6e64d08e82a5fa9a3d3
lesw <lesw1216@gmail.com>
2023-07-17 20:04:43 +0900

    init

---
+++ src/main/java/lesw/howweather/HowWeatherApplication.java
@@ -0,0 +1,13 @@
+package lesw.howweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lesw/howweather/HowWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package lesw.howweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
