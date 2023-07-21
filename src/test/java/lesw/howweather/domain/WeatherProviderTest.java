db51702190a87c4adfb1cd130131f9f97326af8f
lesw <lesw1216@gmail.com>
2023-07-21 18:56:08 +0900

    테스트 수정

--- src/test/java/lesw/howweather/domain/WeatherProviderTest.java
+++ src/test/java/lesw/howweather/domain/WeatherProviderTest.java
@@ -2,15 +2,7 @@ package lesw.howweather.domain;
 
 import lesw.howweather.Entity.Weather;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class WeatherProviderTest {
