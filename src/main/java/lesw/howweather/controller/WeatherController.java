93c43bb1a4f3daaf41ed1f24477f3efea4a309a2
lesw <lesw1216@gmail.com>
2023-07-24 01:26:10 +0900

    WeatherController - RestController로 변경, 반환 값 ResponseEntity<Weather>

--- src/main/java/lesw/howweather/controller/WeatherController.java
+++ src/main/java/lesw/howweather/controller/WeatherController.java
@@ -1,16 +1,21 @@
 package lesw.howweather.controller;
 
+import lesw.howweather.Entity.Weather;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 public class WeatherController {
 
     @GetMapping("/weather")
-    public String GetLocalWeather() {
+    public ResponseEntity<Weather> GetLocalWeather() {
         log.info("GetLocalWeather call");
-        return "/home";
+        return new ResponseEntity<Weather>(new Weather(), HttpStatus.OK);
     }
 }
